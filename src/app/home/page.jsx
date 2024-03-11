4f0bd7027631ab78313668e57c527327ee5754ef
Randula1997 <randularj97@gmail.com>
2024-03-11 22:57:20 +0530

    change the initial page to home

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -1,11 +1,3 @@
-import bg_login from '../../assets/images/home_bg.png';
-import nextIcon from '../../assets/images/next_icon.png';
-import CompanyInfoText from '../(components)/CompanyInfoText';
-import Image from 'next/image';
-import Categorie from '../(components)/Category';
-import Button from '../(components)/Button';
-import Link from 'next/link';
-import NavBar from '../(components)/NavBar';
 import Home from '../page';
 
 function page() {
